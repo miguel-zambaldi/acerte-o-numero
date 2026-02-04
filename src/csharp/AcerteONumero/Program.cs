39952404bad656e8287e119c9f81bbd03512adbd
Bruno Fernandes <bruno.tere@gmail.com>
2026-02-04 13:24:35 -0300

    atualização do programa C# para manter a compatibilidade com programa java

--- src/csharp/AcerteONumero/Program.cs
+++ src/csharp/AcerteONumero/Program.cs
@@ -1,42 +1,42 @@
-﻿namespace com.mycompany.acerteonumero;
+﻿namespace estudos.acerteonumero;
 
 using System;
 
 public class AcerteoNumero
 {
+	private static readonly int NUMERO_MAXIMO = 100;
 
 	public static void Main(String[] args)
 	{
+		Console.WriteLine("Bem-Vindo ao programa Acerte O Numero");
 
-		String respostaUsarNovamente = "sim";
-
+		var respostaUsarNovamente = "s"; // Por padrão eu começo o programa querendo usá-lo!
 		while (respostaUsarNovamente.ToLower().StartsWith("s"))
 		{
-			Console.WriteLine("Bem-Vindo ao programa Acerte O Numero");
-			int numeroAleatorioEscolhido = (int)(Random.Shared.NextDouble() * 50);
-			bool acertou = false;
+			var numeroAleatorioEscolhido = (int)(Random.Shared.NextDouble() * NUMERO_MAXIMO);
 
+			var acertou = false;
 			while (!acertou)
 			{
-				Console.Write("Descubrar o numero escolhido: ");
-				int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Tente descobrir o numero que eu escolhi entre 0 e " + NUMERO_MAXIMO + ": ");
+				var numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
 				if (numeroDigitado == numeroAleatorioEscolhido)
 				{
-					Console.WriteLine("Voce acertou");
+					Console.WriteLine("Voce acertou! Eu escolhi o numero " + numeroAleatorioEscolhido);
 					acertou = true;
 				}
 				else if (numeroDigitado > numeroAleatorioEscolhido)
 				{
-					Console.WriteLine("O numero escolhido e menor");
+					Console.WriteLine("O numero que escolhi e menor (<)");
 				}
 				else
 				{
-					Console.WriteLine("o numero escolhido e maior");
+					Console.WriteLine("O numero que escolhi e maior (>)");
 				}
 			}
 
-			Console.Write("Deseja usar novamente o programa [S = Sim, N = Não]? ");
+			Console.Write("Deseja usar novamente o programa [S = Sim, N = Nao]? ");
 			respostaUsarNovamente = Console.ReadLine();
 		}
 
