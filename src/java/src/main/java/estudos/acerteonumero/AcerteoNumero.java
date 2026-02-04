2fd35e308bec5658fe51eb5cc2adc9f11ecd81c3
Bruno Fernandes <bruno.tere@gmail.com>
2026-02-04 13:06:49 -0300

    organiza os arquivos dos projetos

---
+++ src/java/src/main/java/estudos/acerteonumero/AcerteoNumero.java
@@ -0,0 +1,47 @@
+package estudos.acerteonumero;
+
+import java.util.Scanner;
+
+public class AcerteoNumero
+{
+    private static final int NUMERO_MAXIMO = 100;
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Bem-Vindo ao programa Acerte O Numero");
+        var scanner = new Scanner(System.in);
+
+        var respostaUsarNovamente = "s"; // Por padrão eu começo o programa querendo usá-lo!
+        while(respostaUsarNovamente.toLowerCase().startsWith("s"))
+        {
+            var numeroAleatorioEscolhido = (int)(Math.random() * NUMERO_MAXIMO);
+
+            var acertou = false;
+            while (!acertou)
+            {
+                System.out.print("Tente descobrir o numero que eu escolhi entre 0 e " + NUMERO_MAXIMO + ": ");
+                var numeroDigitado = scanner.nextInt();
+                scanner.nextLine();
+
+                if(numeroDigitado == numeroAleatorioEscolhido)
+                {
+                    System.out.println("Voce acertou! Eu escolhi o numero " + numeroAleatorioEscolhido);
+                    acertou = true;
+                }
+                else if(numeroDigitado > numeroAleatorioEscolhido)
+                {
+                    System.out.println("O numero que escolhi e menor (<)");
+                }
+                else
+                {
+                    System.out.println("O numero que escolhi e maior (>)");
+                }
+            }
+
+            System.out.print("Deseja usar novamente o programa [S = Sim, N = Nao]? ");
+            respostaUsarNovamente = scanner.nextLine();
+        }
+        
+        System.out.println("Fim do programa!");
+    }
+}
