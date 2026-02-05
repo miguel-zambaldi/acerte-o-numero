905398664c21b96261b419967df723d4c814d6a7
Bruno Fernandes <bruno.tere@gmail.com>
2026-02-05 10:49:43 -0300

    implementa a geração de números aleatórios com um número mínimo

--- src/java/src/main/java/estudos/acerteonumero/AcerteoNumero.java
+++ src/java/src/main/java/estudos/acerteonumero/AcerteoNumero.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class AcerteoNumero
 {
+    private static final int NUMERO_MINIMO = 10;
     private static final int NUMERO_MAXIMO = 100;
     
     public static void main(String[] args)
@@ -14,12 +15,12 @@ public class AcerteoNumero
         var respostaUsarNovamente = "s"; // Por padrão eu começo o programa querendo usá-lo!
         while(respostaUsarNovamente.toLowerCase().startsWith("s"))
         {
-            var numeroAleatorioEscolhido = (int)(Math.random() * NUMERO_MAXIMO);
+            var numeroAleatorioEscolhido = NUMERO_MINIMO + (int)(Math.random() * (NUMERO_MAXIMO - NUMERO_MINIMO));
 
             var acertou = false;
             while (!acertou)
             {
-                System.out.print("Tente descobrir o numero que eu escolhi entre 0 e " + NUMERO_MAXIMO + ": ");
+                System.out.print("Tente descobrir o numero que eu escolhi entre " + NUMERO_MINIMO + " e " + NUMERO_MAXIMO + ": ");
                 var numeroDigitado = scanner.nextInt();
                 scanner.nextLine();
 
